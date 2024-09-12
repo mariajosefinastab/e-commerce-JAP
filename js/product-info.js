0433ccdef00a8718edd7c5edf821c1babf56bc80
milanesadeternera <facundojcolombo@gmail.com>
2024-09-11 21:40:57 -0300

    no sabemos leer

--- js/product-info.js
+++ js/product-info.js
@@ -1,17 +1,15 @@
 
-url="https://japceibal.github.io/emercado-api/cats_products/"
+url="https://japceibal.github.io/emercado-api/products/"
 let catName="";
 
 document.addEventListener("DOMContentLoaded", () => {
-  let idCat =  localStorage.getItem('catID');
+  //let idCat =  localStorage.getItem('catID');
   let idProducto = localStorage.getItem('idProducto');
 
-  getJSONData(url+idCat+".json").then(result => {
+  getJSONData(url+idProducto+".json").then(result => {
     if (result.status === "ok") {
-      productos = result.data.products;
-      catName = result.data.catName;
-      console.log(productos);
-      displayProduct(productos, idProducto);
+      producto = result.data;
+      displayProduct(producto, idProducto);
     } else {
       alert("Error al cargar contenido");
     }
@@ -19,43 +17,60 @@ document.addEventListener("DOMContentLoaded", () => {
 
 });
 
-function displayProduct(products, idProducto){
+function displayProduct(producto, idProducto){
   //Aca desplegas el diseño
-  console.log(products);
-  const productoAMostrar = products.find(p => p.id === parseInt(idProducto));
+  console.log(producto);
   let content = 
-     ` 
-    <!-- Categoría -->
-    <div class="container">
-      <div class="row text-start fs-3" >
-        <p>${catName}</p>
-        <hr> 
-      </div>
-    </div>
-    <!-- Productos -->
-    <div class="container">
-      <div class="row">
-        <!--imagen-->
-        <div class="col-8 d-flex justify-content-start">
-          <img src="${productoAMostrar.image}" class="productImg" alt="...">
-        </div>
-        <div class="col-4">
-          <!--Nombre y descripcion-->
-          <div class='my-5'>
-            <h2 class="fs-1 text">${productoAMostrar.name}</h2>
+     `<!-- Categoría -->
+        <div class="container">
+          <div class="row text-start fs-3" >
+            <p>${catName}</p>
+            <hr> 
           </div>
-          <div class="row mt-2 text-start">
-            <!--Precio-->
-            <p class="col fs-3 text">$${productoAMostrar.cost}${productoAMostrar.currency}</p>
-            <!--Unidades vendidas-->
-            <p class="col fs-3 text" >Vendidos: ${productoAMostrar.soldCount}</p>
-          </div>
-          <p class="fs-5 text text-start mt-5">${productoAMostrar.description}</p>
         </div>
-      </div>
-    </div>
-    `;
-  
+        <!-- Productos -->
+        <div class="container">
+          <div class="row">
+            <div class="col">
+            <! -- carousel -->
+            <div id="carouselImagenes" class="carousel slide">
+              <div id="carousel" class="carousel-inner">`;
 
+              producto.images.forEach(image => {
+                content +=`<div class="carousel-item">
+                  <img src="${image}" class="d-block w-100">
+                </div>`;
+              });
+
+               content +=`</div>
+              <button class="carousel-control-prev" type="button" data-bs-target="#carouselImagenes" data-bs-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Previous</span>
+              </button>
+              <button class="carousel-control-next" type="button" data-bs-target="#carouselImagenes" data-bs-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="visually-hidden">Next</span>
+              </button>
+            </div>
+            </div>
+            <!-- Segunda columna -->
+            <div class="col-4">
+            <!--Nombre y descripcion-->
+            <div class='my-5'>
+              <h2 class="fs-1 text">${producto.name}</h2>
+            </div>
+            <div class="row mt-2 text-start">
+              <!--Precio-->
+              <p class="col fs-3 text">$${producto.cost}${producto.currency}</p>
+              <!--Unidades vendidas-->
+              <p class="col fs-3 text" >Vendidos: ${producto.soldCount}</p>
+            </div>
+          <p class="fs-5 text text-start mt-5">${producto.description}</p>
+        </div>
+      </div>
+    </div>`;
+  
   document.getElementById("product-info").innerHTML = content; 
+  document.getElementsByClassName("carousel-item")[0].classList.add("active");
+
 }
