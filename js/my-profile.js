957cee505eb1f5839169cf95b48ac37822f77006
Maria Josefina Stabile <mariajosefinastabile@gmail.com>
2024-10-11 12:57:13 -0300

    Cambios de id button y js

--- js/my-profile.js
+++ js/my-profile.js
@@ -1,39 +1,48 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const form = document.getElementById('registroForm');
-    const nombreInput = document.getElementById('nombre');
-    const apellidoInput = document.getElementById('apellido');
-    const emailInput = document.getElementById('email');
-  
-    button.addEventListener('click', function(event) {
-      /* event.preventDefault(); */
-  
+  const form = document.getElementById('registroForm');
+  const nombreInput = document.getElementById('nombre');
+  const apellidoInput = document.getElementById('apellido');
+  const emailInput = document.getElementById('email');
+  const button = document.getElementById('guardarBtn');
+
+  button.addEventListener('click', function() {
+    let isValid = true;
+
 
     if (nombreInput.checkValidity()) {
-        nombreInput.classList.remove('is-invalid');
-        nombreInput.classList.add('is-valid');
-      } else {
-        nombreInput.classList.remove('is-valid');
-        nombreInput.classList.add('is-invalid');
-      }
-  
+      nombreInput.classList.remove('is-invalid');
+      nombreInput.classList.add('is-valid');
+    } else {
+      nombreInput.classList.remove('is-valid');
+      nombreInput.classList.add('is-invalid');
+      isValid = false;
+    }
 
     if (apellidoInput.checkValidity()) {
-        apellidoInput.classList.remove('is-invalid');
-        apellidoInput.classList.add('is-valid');
-      } else {
-        apellidoInput.classList.remove('is-valid');
-        apellidoInput.classList.add('is-invalid');
-      }
-  
+      apellidoInput.classList.remove('is-invalid');
+      apellidoInput.classList.add('is-valid');
+    } else {
+      apellidoInput.classList.remove('is-valid');
+      apellidoInput.classList.add('is-invalid');
+      isValid = false;
+    }
+
 
     if (emailInput.checkValidity()) {
-        emailInput.classList.remove('is-invalid');
-        emailInput.classList.add('is-valid');
-      } else {
-        emailInput.classList.remove('is-valid');
-        emailInput.classList.add('is-invalid');
-      }
-    });
+      emailInput.classList.remove('is-invalid');
+      emailInput.classList.add('is-valid');
+    } else {
+      emailInput.classList.remove('is-valid');
+      emailInput.classList.add('is-invalid');
+      isValid = false;
+    }
+
+
+    if (isValid) {
+
+      console.log('Formulario válido');
+    }
+  });
 });
 
 
