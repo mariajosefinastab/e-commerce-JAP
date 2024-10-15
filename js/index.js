6b4b6aa97f8bdde77f3f737c9cb3dcf4ea2f2685
Wanheda29 <faustofe29@gmail.com>
2024-10-15 17:35:45 -0300

    menu-desplegable

--- js/index.js
+++ js/index.js
@@ -11,4 +11,38 @@ document.addEventListener("DOMContentLoaded", function(){
         localStorage.setItem("catID", 103);
         window.location = "products.html"
     });
+
+    document.getElementById("cerrar-sesion").addEventListener("click", (event)=>{
+        let email = localStorage.getItem("email");
+        email = ""
+        document.getElementById("user-email").innerHTML = ""
+        window.location = "login.html"
+        console.log(email);
+
+    });
+
+
 });
+
+
+document.getElementById("user-email").addEventListener("click", function(event) {
+    //event.preventDefault(); // Evita el comportamiento por defecto del enlace
+    var dropdown = document.getElementById("dropdown-menu");
+    dropdown.classList.toggle("show");
+});
+
+// Cierra el menú si se hace clic fuera del mismo
+window.onclick = function(event) {
+    if (!event.target.matches('#user-email')) {
+        var dropdowns = document.getElementsByClassName("dropdown-menu");
+        for (var i = 0; i < dropdowns.length; i++) {
+            var openDropdown = dropdowns[i];
+            if (openDropdown.classList.contains('show')) {
+                openDropdown.classList.remove('show');
+            }
+        }
+    }
+}
+
+
+//TENGO QUE CONTINUAR AGREGANDO LA FUNCIONALIDAD DE LAS OPCIONES, REDIRIGIENDO HACIA LAS PAGINAS CORRESPONIENTES
