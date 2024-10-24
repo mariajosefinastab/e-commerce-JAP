868ce38e1a76560a93b650f5fb7c508fb48fd6a3
Maria Josefina Stabile <103007935+mariajosefinastab@users.noreply.github.com>
2024-10-24 08:52:58 -0300

    Update init.js

--- js/init.js
+++ js/init.js
@@ -7,6 +7,39 @@ const CART_INFO_URL = "https://japceibal.github.io/emercado-api/user_cart/";
 const CART_BUY_URL = "https://japceibal.github.io/emercado-api/cart/buy.json";
 const EXT_TYPE = ".json";
 
+
+let carrito = {
+  items: []
+};
+
+document.addEventListener("DOMContentLoaded", ()=>{
+  user = getUser();
+  carrito = JSON.parse(localStorage.getItem("carrito")) || { items: [] }; // Cargar carrito
+  if(document.body.className != user.theme){
+    tema();
+  }
+
+})
+
+//functions del User
+
+function getCarrito(){ //devuelve todos los elementos del carrito
+  return carrito.items;
+}
+
+function addCarrito(productoComprado) { // agrega un producto al carrito y actualiza localstorage
+  let existingItem = carrito.items.find(item => item.id === productoComprado.id);
+
+  if(existingItem) {
+    existingItem.cantidad += 1;
+  } else {
+    carrito.items.push(productoComprado);
+  }
+
+  localStorage.setItem("carrito", JSON.stringify(carrito));
+}
+
+
 let showSpinner = function(){
   document.getElementById("spinner-wrapper").style.display = "block";
 }
