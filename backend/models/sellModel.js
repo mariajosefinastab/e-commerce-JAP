4c3934c1506d3a5d35e2a4c06e6261afdb14faf0
mariajosefinastab <mariajosefinastabile@gmail.com>
2024-11-29 09:32:14 -0300

    Backend

--- backend/models/sellModel.js
+++
@@ -1,8 +0,0 @@
-const { listJsonFiles, getJsonFromFile } = require("./jsonModel");
-
-const folder = "sell";
-
-const listSales = () => listJsonFiles(folder);
-const getSale = (id) => getJsonFromFile(folder, `${id}.json`);
-
-module.exports = { listSales, getSale };
--- backend/routes/userCartRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const userCartController = require("../controllers/userCartController");
-
-router.get("/", userCartController.listUserCarts);
-router.get("/:id", userCartController.getUserCart);
-
-module.exports = router;
